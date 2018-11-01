0e2bca5961a71d4590d27a92456149ac7e3a2555
Apollo Tang <tang.apollo@gmail.com>
2018-11-01 00:18:54 -0400

    initial commit

---
+++ aliases_device.sh
@@ -0,0 +1,20 @@
+# # Debug Messsages -----------------------------------------------
+# echo 'echo from ~/1/1-sy/configurations/bash/lights/aliases_device.sh'
+
+################################################
+#
+
+#/ [Application] --------------------------------
+
+#alias chromium='/Applications/Chromium.app/Contents/MacOS/Chromium &'
+alias chrome='/usr/bin/open -a "/Applications/Google Chrome.app"'
+alias chrome1="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory=\"Default\""
+alias chrome2="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory=\"Profile 1\""
+
+
+alias run-vendor-server="cd ~/0/z-local/vendor-server/; ./start.sh"
+alias run-labRunner1="cd ~/0/z-local/labs-runner-3030/; ./start.sh"
+alias run-labRunner2="cd ~/0/z-local/labs-runner-3040/; ./start.sh"
+
+################################################
+#echo '.bash_aliases_device_lights reading done'
---
+++ bashrc_device.sh
@@ -0,0 +1,75 @@
+# # Debug Messsages -----------------------------------------------
+# echo 'echo from ~/1/1-sy/configurations/bash/lights/bashrc_device.sh'
+
+export IAM=lights
+export PLATFORM=mac
+export RIPGREP_CONFIG_PATH="/Users/apollotang/1/1-sy/configurations/ripgrep/dot.ripgreprc"
+
+
+### Enable Glob support
+#/ https://gist.github.com/reggi/475793ea1846affbcfe8
+shopt -s globstar
+
+
+### Path
+#/
+#/ @NOTE: reverse order: the last one will be prepended to the path
+#/
+PATH="$HOME/script/bin2":$PATH
+PATH="$HOME/script/bin":$PATH
+PATH="$HOME/bin":$PATH
+PATH="$HOME/1/1-sy/scripts/bin":$PATH                       # custom scripts link to here
+PATH="$HOME/Applications/kdiff3.app/Contents/MacOS":$PATH   # kdiff3
+# PATH=/Users/apollotang/opt/local/anaconda2/bin:$PATH
+# PATH=/Users/apollotang/opt/local/anaconda3/bin:$PATH
+PATH=/usr/local/Cellar/coreutils/8.30/bin:$PATH             # manually link coreutil b/c gsync has already symlinc to /Library/Application\ Support/GoodSync/gsync
+#/
+#/ @NOTE: normal order: the last one will be appended to the path
+#/
+PATH=$PATH:"$HOME/opt/local/scala-2.11.2/bin"
+PATH=$PATH:"/usr/local/Cellar/neovim-dot-app/HEAD/bin/gnvim"
+PATH=$PATH:"$HOME/opt/devtools/autotools-bin/bin"
+export PATH
+
+
+###
+#/ Ruby
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+
+###
+#/ Android
+export ANDROID_HOME=${HOME}/Library/Android/sdk
+export PATH=${PATH}:${ANDROID_HOME}/emulator  # this must be before tools, else wrong emulator will be evoke
+export PATH=${PATH}:${ANDROID_HOME}/tools
+export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+
+
+###
+#/ for nvm
+export NVM_DIR="/Users/apollotang/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+
+###
+#/ Scala
+export SCALA_HOME="$HOME/opt/local/scala-2.11.2/bin"
+
+
+###
+#/ local directory
+export dir_vendor_server="/Users/apollotang/0/z-local/vendor-server/"
+
+
+###
+#/ Import configuration that change frequently or private
+if [ -f "$dir_config_device/git-sub-local/local-only.sh" ]; then
+  . "$dir_config_device/git-sub-local/local-only.sh"
+fi
+
+
+###/ Run things
+# archey
+
+###################################
+#echo '.bashrc_lights reading done'
