ded30135442081463d935a049e9a3d5618a65c1c
Apollo Tang <tang.apollo@gmail.com>
2018-12-15 14:50:32 -0500

    improve workflow for lab runner

--- bashrc_device_proj.sh
+++ bashrc_device_proj.sh
@@ -13,41 +13,93 @@ alias chrome2="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --
 
 
 
-alias run-vendor-server="cd ~/0/z-local/vendor-server/; ./start.sh"
+#/ [run vendor] -------------------------------
+  alias rrrVendorServer="cd ~/0/z-local/vendor-server/; ./start.sh"
+  alias gggVendorServer="cd ~/0/z-local/vendor-server/"
+  alias gggLabTemplates="cd ~/0/z-local/vendor-server/html-template"
+  function eeeLabSciptTags {
+    local dir='/Users/apollotang/0/z-local/vendor-server'
+    local fullPathToFile="$dir/script-tags.html"
+    (cd $dir && mvim $fullPathToFile)
+  }
+  function copyHere_labTemplates_react {
+    local from="/Users/apollotang/0/z-local/vendor-server/html-template/react-template.html"
+    local to="./react-template-$(date +"%Y%m%d%H%M").html"
+    cp $from $to
+  }
+  function copyHere_labTemplates_general {
+    local from="/Users/apollotang/0/z-local/vendor-server/html-template/general-template.html"
+    local to="./general-template-$(date +"%Y%m%d%H%M").html"
+    cp $from $to
+  }
 
 
+#/ [lab runner 3331] { ---------------------------
+  function eeeLabRunner1_desktopLab {
+    local dir='/Users/apollotang/0/z-local/labs-runner-3331'
+    local fullPathToFile="$dir/start.sh"
+    (cd $dir && mvim $fullPathToFile)
+  }
+  function rrrLabRunner1_desktopLab {
+    local dir='/Users/apollotang/0/z-local/labs-runner-3331'
+    local file=start.sh
+    cd $dir && ./$file
+  }
+  function gggLabRunner1_desktopLab {
+    local dir='/Users/apollotang/0/z-local/labs-runner-3331'
+    cd $dir
+  }
+  function gggLab1_desktopLab {
+    local dir='/Users/apollotang/Desktop/lab'
+    cd $dir
+  }
 
-#/ [lab runner 3040] ---------------------------
-function eeeLabRunner3040 {
-  local dir='/Users/apollotang/0/z-local/labs-runner-3040'
-  local fullPathToFile="$dir/start.sh"
-  (cd $dir && mvim $fullPathToFile)
-}
-function sssLabRunner3040 {
-  local dir='/Users/apollotang/0/z-local/labs-runner-3040'
-  local file=start.sh
-  cd $dir && ./$file
-}
-function gggLabRunner3040 {
-  local dir='/Users/apollotang/0/z-local/labs-runner-3040'
-  cd $dir
-}
 
-#/ [lab runner 3030] ---------------------------
-function eeeLabRunner3030 {
-  local dir='/Users/apollotang/0/z-local/labs-runner-3030'
-  local fullPathToFile="$dir/start.sh"
-  (cd $dir && mvim $fullPathToFile)
-}
-function sssLabRunner3030 {
-  local dir='/Users/apollotang/0/z-local/labs-runner-3030'
-  local file=start.sh
-  cd $dir && ./$file
-}
-function gggLabRunner3030 {
-  local dir='/Users/apollotang/0/z-local/labs-runner-3030'
-  cd $dir
-}
+#/ [lab runner 3332] ---------------------------
+#/
+  function eeeLabRunner2 {
+    local dir='/Users/apollotang/0/z-local/labs-runner-3332'
+    local fullPathToFile="$dir/start.sh"
+    (cd $dir && mvim $fullPathToFile)
+  }
+  function rrrLabRunner2 {
+    local dir='/Users/apollotang/0/z-local/labs-runner-3332'
+    local file=start.sh
+    cd $dir && ./$file
+  }
+  function gggLabRunner2 {
+    local dir='/Users/apollotang/0/z-local/labs-runner-3332'
+    cd $dir
+  }
+  #/ no goto command because this can change from
+  #/ folder to folder, thus need to configured manually
+  #/
+  # function gggLab2 {
+  #   local dir='/Users/apollotang/0/g-labs/labs--external-resource/src-3332'
+  #   cd $dir
+  # }
+
+
+#/ [lab runner 3333] ---------------------------
+#/
+  function eeeLabRunner3 {
+    local dir='/Users/apollotang/0/z-local/labs-runner-3333'
+    local fullPathToFile="$dir/start.sh"
+    (cd $dir && mvim $fullPathToFile)
+  }
+  function rrrLabRunner3 {
+    local dir='/Users/apollotang/0/z-local/labs-runner-3333'
+    local file=start.sh
+    cd $dir && ./$file
+  }
+  function gggLabRunner3 {
+    local dir='/Users/apollotang/0/z-local/labs-runner-3333'
+    cd $dir
+  }
+  function gggLab3 {
+    local dir='/Users/apollotang/0/g-labs/labs--external-resource/src-3333'
+    cd $dir
+  }
 
 
 
