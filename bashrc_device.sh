8e429b0c480c1ec214c0e813adac65fdbbbf7e89
Apollo Tang <tang.apollo@gmail.com>
2020-01-09 06:39:36 -0500

    work remove unused

--- bashrc_device.sh
+++ bashrc_device.sh
@@ -1,11 +1,9 @@
 # # Debug Messsages -----------------------------------------------
 # echo 'echo from ~/1/1-sy/configurations/bash/lights/bashrc_device.sh'
 
-export IAM=lights
+export IAM=work
 export PLATFORM=mac
 export RIPGREP_CONFIG_PATH="/Users/apollo.tang/1/1-sy/configurations/ripgrep/dot.ripgreprc"
-# export VIM_IN_MACVIM_BIN="/usr/local/Cellar/macvim/HEAD-79541d2_2/MacVim.app/Contents/MacOS/Vim"
-# export ONI_NEOVIM_PATH="/usr/local/Cellar/neovim/0.3.4/bin/nvim"
 
 
 ### Enable Glob support
@@ -50,27 +48,9 @@ export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
 
 ###
-#/ Android
-export ANDROID_HOME=${HOME}/Library/Android/sdk
-export PATH=${PATH}:${ANDROID_HOME}/emulator  # this must be before tools, else wrong emulator will be evoke
-export PATH=${PATH}:${ANDROID_HOME}/tools
-export PATH=${PATH}:${ANDROID_HOME}/platform-tools
-
-
-###
-#/ for nvm
-# export NVM_DIR="/Users/apollo.tang/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-#
-
-###
-#/ Scala
-export SCALA_HOME="$HOME/opt/local/scala-2.11.2/bin"
-
-
-###
 #/ local directory
 export dir_vendor_server="/Users/apollo.tang/0/z-local/vendor-server/"
+
 
 ###
 #/ sqlite installed via brew
